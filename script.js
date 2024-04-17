cc01906ade6024f46fc2bc43fe525cf2005d0123
@iLeSoleil <jux.till@icloud.com>
2024-04-17 10:58:38 +0200

    Update static content

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function toggleAnswer(element) {
     }
 }
 
+function toggleVisibility(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === "none") {
+        element.style.display = "block";  // Show the element
+    } else {
+        element.style.display = "none";   // Hide the element
+    }
+}
